2bd7aa4d66e2d9bd8f74e6671afe60dba01f0c64
illusion030 <illusion030@gmail.com>
2017-10-21 15:43:14 +0800

    Use semantic ui

--- src/app.js
+++ src/app.js
@@ -2,19 +2,16 @@ var info = true
 $(document).ready( function() {
 	$('.infobtn').click( function() {
 		if (!info) {
-			$('.info').animate({
-			'width': '100%'
-			}, 1000, function(){
-			});
+			$('.info').transition('slide right');
+			$('.contact').transition('slide left');
 			info = true;
 		}
+		
 	});
 	$('.contactbtn').click( function() {
 		if (info) {
-			$('.info').animate({
-			'width': '0px'
-			}, 1000, function(){
-			});
+			$('.info').transition('slide right');
+			$('.contact').transition('slide left');
 			info = false;
 		}
 	});
