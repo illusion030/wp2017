a0c2e1b209478780a31b3e0bd6d454a25b197889
illusion030 <illusion030@gmail.com>
2017-11-22 13:51:34 +0800

    Add ajax

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 var info = true
+var contact = false
+var game = false
+function escapeHtml(data) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+
+  return data.replace(/[&<>"']/g, function(m) { return map[m]; })
+}
+
 $(document).ready( function() {
 	$('.infobtn').click( function() {
-		if (!info) {
+		if (contact) { 
 			$('.info').transition('slide right');
 			$('.contact').transition('slide left');
-			info = true;
+      contact = false;
+      info = true;
+    } else if (game) {
+			$('.info').transition('slide left');
+			$('.game').transition('slide right');
+			game = false;
+      info = true;
 		}
 		
 	});
@@ -13,6 +33,44 @@ $(document).ready( function() {
 			$('.info').transition('slide right');
 			$('.contact').transition('slide left');
 			info = false;
-		}
+      contact = true;
+		} else if (game) {
+			$('.game').transition('slide right');
+			$('.contact').transition('slide left');
+			game = false;
+      contact = true;
+    }
 	});
+  $('.alert').click( function() {
+    
+		if (info) {
+			$('.game').transition('slide left');
+			$('.info').transition('slide right');
+			info = false;
+      game = true;
+		} else if (contact) {
+			$('.game').transition('slide right');
+			$('.contact').transition('slide left');
+			contact = false;
+      game = true;
+    }
+
+    $('.game').html('wait...');
+
+    var name;
+    name = escapeHtml($('.inputText').val());
+    $.ajax({
+    url: 'src/ajax.php',
+    cache: false,
+    type: 'POST',
+    data: { data: name},
+    error: function(xhr) {
+      alert('Ajax request error');
+    },
+    success: function(res) {
+      $('.game').html(res);
+    }
+    
+    });
+  });
 });
